0e9a7ef4fa9c27d9a7e9942f26bc511bc90e5c7b
Hannah McLaughlin <hannah.mclaughlin@oxionics.com>
2021-12-07 15:17:02 +0000

    SYS-179 Use git hash and distance from root in version

--- before_poetry_build.py
+++
@@ -1,9 +0,0 @@
-from os import path
-
-import versioneer
-
-version = versioneer.get_version()
-versioneer.write_to_version_file(
-    path.join(path.dirname(path.realpath(__file__)), "artiq", "_version.py"),
-    version,
-)
