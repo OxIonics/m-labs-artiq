4db629ec80df54393b3a040bef4b4ecc7bc50c8e
Michael Birtwell <michael.birtwell@oxionics.com>
2022-04-25 12:45:32 +0100

    Expose the dynamic version to the software

--- artiq/_version.py
+++ artiq/_version.py
@@ -1,2 +1,21 @@
+_DUMMY_VERSION = "7.unknown.beta"
+# We need the double quotes to generate a valid line after
+# poetry-dynamic-versioning has done a find and replace
+__version__ = f"{_DUMMY_VERSION}"
+
+
 def get_version():
-    return "7.unknown.beta"
+    global __version__
+
+    if __version__ != _DUMMY_VERSION:
+        # poetry-dynamic-versioning has run and set the version correctly
+        # or we've updated this below
+        return __version__
+
+    try:
+        import poetry_dynamic_versioning
+    except ImportError:
+        return _DUMMY_VERSION
+
+    __version__ = poetry_dynamic_versioning._get_version(poetry_dynamic_versioning.get_config(__file__))
+    return __version__
