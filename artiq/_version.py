2ad76267fcc30acd14cc614550c5b619eabee03d
Michael Birtwell <michael.birtwell@oxionics.com>
2022-04-25 18:22:55 +0100

    Getting the dynamic version from a path/git dependency
    If we want the dynamic version from a path/git dependency in another package
    we need to make sure we run poetry_dynamic_versioning in the correct
    working directory and install poetry_dynamic_versioning and path_version in
    that package. All a bit yuck.

--- artiq/_version.py
+++ artiq/_version.py
@@ -1,3 +1,5 @@
+import os
+
 _DUMMY_VERSION = "7.unknown.beta"
 # We need the double quotes to generate a valid line after
 # poetry-dynamic-versioning has done a find and replace
@@ -12,10 +14,14 @@ def get_version():
         # or we've updated this below
         return __version__
 
+    cwd = os.getcwd()
     try:
         import poetry_dynamic_versioning
+        os.chdir(os.path.dirname(os.path.dirname(__file__)))
         __version__ = poetry_dynamic_versioning._get_version(poetry_dynamic_versioning.get_config(__file__))
     except Exception:
         pass
+    finally:
+        os.chdir(cwd)
 
     return __version__
