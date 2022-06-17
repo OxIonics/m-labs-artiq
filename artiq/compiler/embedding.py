fe8823fedfd5ede12dd3213f3e8150e49882974f
Michael Birtwell <michael.birtwell@oxionics.com>
2022-06-17 17:03:12 +0100

    Add comments on `_check_core`

--- artiq/compiler/embedding.py
+++ artiq/compiler/embedding.py
@@ -772,6 +772,16 @@ class Stitcher:
         self._core_name = None
 
     def _check_core(self, core_name):
+        """ Check whether `core_name` is the name of the core we're compiling for
+        """
+        # `Core` objects don't know their own name so we can't simply compare
+        # against the name passed unless we've found out the name first.
+        # If we don't yet know the name of the core we're compiling for we try
+        # and fetch the core from the device manager and compare the resulting
+        # object. If that succeeds we now know the name of the core we're
+        # compiling for and can cache that to speed up later uses.
+        # Seen as this check rarely fails and the device manager is probably a
+        # network hop away, this can achieve significant speedups.
         if self._core_name is not None:
             return self._core_name == core_name
 
